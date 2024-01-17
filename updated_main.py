943eb7c6576516fbe6e4bf57f4292d18f447bcbf
ssingh55 <suvamsingh55@gmail.com>
2024-01-18 00:44:53 +0530

    Update updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -1,7 +1,7 @@
 import os
 
 TEST_SECRET = os.environ["test_secret"]
-Test_secret = os.getenv["test_secret"]
+Test_secret = os.getenv("test_secret")
 
 print(f"The client secret is: {TEST_SECRET}")
 print(f"The client secret 2 is: {Test_secret}")
