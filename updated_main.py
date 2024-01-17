b9a1c1efe2cce175c84a073fa503ce254b6ad7b7
ssingh55 <suvamsingh55@gmail.com>
2024-01-18 01:03:51 +0530

    added if else to verify the key

--- updated_main.py
+++ updated_main.py
@@ -3,5 +3,12 @@ import os
 TEST_SECRET = os.environ["test_secret"]
 Test_secret = os.getenv("test_secret")
 
-print(f"The client secret is: {TEST_SECRET}")
-print(f"The client secret 2 is: {Test_secret}")
+if (TEST_SECRET=="HelloWorld" and Test_secret == "HelloWorld"):
+  print(f"The client secret is: {TEST_SECRET}")
+  print(f"The client secret 2 is: {Test_secret}"):
+elif (TEST_SECRET=="HelloWorld"):
+  print(f"The client secret is: {TEST_SECRET}")
+elif (Test_secret == "HelloWorld"):
+  print(f"The client secret 2 is: {Test_secret}")
+else:
+  print(f"Test secret doesnt matches")
