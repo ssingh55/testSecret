b749315c107a5e963add01d11a1d8930ac10ba77
ssingh55 <suvamsingh55@gmail.com>
2024-01-18 01:04:57 +0530

    Update updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -5,7 +5,7 @@ Test_secret = os.getenv("test_secret")
 
 if (TEST_SECRET=="HelloWorld" and Test_secret == "HelloWorld"):
   print(f"The client secret is: {TEST_SECRET}")
-  print(f"The client secret 2 is: {Test_secret}"):
+  print(f"The client secret 2 is: {Test_secret}")
 elif (TEST_SECRET=="HelloWorld"):
   print(f"The client secret is: {TEST_SECRET}")
 elif (Test_secret == "HelloWorld"):
